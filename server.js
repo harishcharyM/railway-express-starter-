684023458c0f5cf731e431e2256487bd89bce150
hari-siri <96523426+harishcharyM@users.noreply.github.com>
2025-12-01 19:44:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
 let appName = process.env.APP_NAME || 'Railway Node Root Starter';
 
 app.use(morgan('dev'));
